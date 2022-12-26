59c752ae154c38ece765f0e3cde2d9dab37b463a
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2022-12-26 20:24:17 +0100

    UI and pushed DatePicker to the right using HStack and Spacer()

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
 //    @State private var wakeUp = Date.now
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 0
+    @State private var coffeeAmount = 1
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
@@ -27,23 +27,40 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
+//                VStack(alignment: .leading, spacing: 0) {
+                Section {
+//                    Text("When do you want to wake up?")
+//                        .font(.headline)
+                    HStack {
+                        Spacer()
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
+                } header: {
                     Text("When do you want to wake up?")
-                        .font(.headline)
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
+                        .foregroundColor(.black)
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
+//                VStack(alignment: .leading, spacing: 0) {
+                Section {
+//                    Text("Desired amount of sleep")
+//                        .font(.headline)
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                        .foregroundColor(.accentColor)
+                } header: {
+                    Text("Desired amount of sleep")
+                        .foregroundColor(.black)
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
+//                VStack(alignment: .leading, spacing: 0)  {
+                Section {
+//                    Text("Daily coffee intake")
+//                        .font(.headline)
                     Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 0...20)
+                        .foregroundColor(.accentColor)
+                } header: {
+                    Text("Daily coffee intake")
+                        .foregroundColor(.black)
                 }
             }
             .navigationTitle("BetterRest")
@@ -51,7 +68,7 @@ struct ContentView: View {
                 Button("Calculate", action: calculateBedtime)
             }
             .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
+                Button("OK") { }.foregroundColor(.accentColor)
             } message: {
                 Text(alertMessage)
             }
